5cfa364d9c02c6c83bc2022474790c357f7c3c1f
alexy piche <alexyautumn@gmail.com>
2023-11-09 18:47:42 -0500

    header section done

--- yourbank_react/src/assets/index.js
+++ yourbank_react/src/assets/index.js
@@ -1,5 +1,11 @@
 import logo from "./Logo.png";
+import header_img from "./header_img.png";
+import abstract1 from "./abstract1.png";
+import check_icon from "./check_icon.png";
 
 export {
-    logo
+    logo,
+    header_img,
+    abstract1,
+    check_icon
 };
--- yourbank_react/src/components/Header.jsx
+++ yourbank_react/src/components/Header.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React from 'react';
+
+import { header_img, abstract1, check_icon } from '../assets';
+import { Button } from '../components';
+
+import './header.css'
+
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <section id='home' className='app__header'>
+      <img src={abstract1} alt="abstract" className='background_img' />
+      <div className='app__header-info'>
+        <div className='app__header-info_aside'>
+          <img src={check_icon} alt="check icon" />
+          <p>No LLC Required, No Credit Check.</p>
+        </div>
+        <h1>
+          Welcome to YourBank
+          Empowering Your <span>Financial Journey</span>
+        </h1>
+        <p>At YourBank, our mission is to provide comprehensive banking solutions that empower individuals and businesses to achieve their financial goals. We are committed to delivering personalized and innovative services that prioritize our customers' needs.</p>
+        <Button content="Open Account"/>
+      </div>
+
+      <div className='app__header-img'>
+        <img src={header_img} alt="header image" />
+      </div>
+    </section>
   )
 }
 
